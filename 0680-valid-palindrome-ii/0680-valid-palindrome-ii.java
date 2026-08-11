42db4a40d3be0cd518960362075f91e22ca687a5
Vishnu prakash <vishnuprakash.it@gmail.com>
2026-08-11 18:25:18 +0530

    Time: 4 ms (99.05%), Space: 47.6 MB (86.07%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -4,16 +4,16 @@ class Solution {
         int right = s.length() - 1;
         while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
-                return isPalindrome(s, left + 1, right) ||
-                        isPalindrome(s, left, right - 1);
+                return isPalindrome(s, left + 1, right)
+                        || isPalindrome(s, left, right - 1);
             }
-            right--;
             left++;
+            right--;
         }
         return true;
     }
 
-    private boolean isPalindrome(String s, int left, int right) {
+    public boolean isPalindrome(String s, int left, int right) {
         while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
                 return false;
